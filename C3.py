120ef63d966353091104f07ca8676c438d50fa24
JimmyBowden <32491606+JimmyBowden@users.noreply.github.com>
2018-02-01 00:35:05 -0500

    Update C3.py

--- C3.py
+++ C3.py
@@ -2,3 +2,55 @@
 #Jimmy Bowden
 #IT780
 
+#------------Variables-----------------
+
+
+
+#------------Functions-----------------
+def getInput():
+    while True:
+        try:
+            inVal = input("Enter a sentence: ")
+            if inVal.strip() != "":
+                pass
+            else:
+                raise ValueError("There was nothing entered.")
+
+            return inVal
+            break
+        except Exception as error:
+            print(error)
+
+
+def makeDictionary(inString):
+    myDict = {}
+    for char in inString:
+        if char in myDict:
+            myDict[char] += 1
+        elif char == " ":
+            pass
+        else:
+            myDict[char] = 1
+    return myDict
+
+
+def orderDictionary(myDict):
+    myDictList = []
+    for key in myDict:
+        temp = ()
+        temp = [myDict[key], key]
+        myDictList.append(temp)
+    sortedDictList = sorted(myDictList, reverse=True)
+    #print(sortedDictList)
+    return sortedDictList
+
+
+def printDictionary(myDictList):
+    for i in myDictList:
+        print(i[1] + ": " + str(i[0]))
+
+#------------Main----------------------
+myInput = getInput()
+myDict = makeDictionary(myInput)
+myDictList = orderDictionary(myDict)
+printDictionary(myDictList)
