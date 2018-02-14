783807745b262611802837973fcd00e9e23f6f03
JimmyBowden <32491606+JimmyBowden@users.noreply.github.com>
2018-02-13 19:31:08 -0500

    Update C7.py

--- C7.py
+++ C7.py
@@ -9,26 +9,27 @@ import csv
 html = urlopen("http://www.showmeboone.com/sheriff/jailresidents/")
 #soup = BeautifulSoup(response.text, "lxml")
 bsObj = BeautifulSoup(html, "html.parser")
+outputFile = 'C7Output.csv'
 
 
 #---------------------Functions
 def parseTable():
     infoTable = bsObj.find("tbody", {"id" : "mrc_main_table"})
-    #print(infoTable)
     rows = infoTable.findAll("tr")
     rowList = []
-    #print(rows[0])
     for item in rows:
+        rowTuple = ()
         rowString = ""
         for cell in item.findAll("td"):
             rowString = rowString + cell.text.strip() + ","
         rowString = rowString[:-1]
+        rowString = rowString + "\n"
         rowList.append(rowString)
-    with open("C7Output.csv", 'wb') as file:
-        writer = csv.writer(file, lineterminator='\n')
-        #writer.writerows(rowList)
-        for item in rowList:
-            writer.writerow([item])
+    with open(outputFile, 'w', encoding="utf-8") as output:
+        writer = csv.writer(output, dialect='excel', delimiter=",")
+        for row in rowList:
+            cellsList = tuple(filter(None, row.split(',')))
+            writer.writerow(cellsList)
     #for line in rows:
         #print(line.text.strip())
 
