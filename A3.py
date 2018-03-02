c7a1301c0c980dc7a25425596def1e447728cfa3
JimmyBowden <32491606+JimmyBowden@users.noreply.github.com>
2018-03-02 09:52:40 -0500

    Update A3.py

--- A3.py
+++ A3.py
@@ -4,7 +4,7 @@
 
 
 import csv
-#import C8
+from C8 import myRecommender
 
 #-----------------------------------Variables
 
@@ -14,43 +14,54 @@ import csv
 def readBookCSV():
     #need to extract isbn and title
     myDict = {}
+    isbnList =[]
     fName = 'BX-Books.csv'
     with open(fName, 'r') as inFile:
         line = inFile.readline()
-        #while line != "":
-        count = 0
-        while count <1001:
-            nameList  = line.split(";")
-            print(nameList)
+        while line != "":
+        #count = 0
+        #while count <50001:
+            cleanLine = str.replace(line, '"', "")
+            nameList  = cleanLine.split(";")
+            #print(nameList)
+            #if nameList[0] != '"ISBN"':
             myDict[nameList[0]] = nameList[1]
             line = inFile.readline()
-            count += 1
+            #count += 1
     #print(myList)
     return myDict
 
-def readRatingCSV(nameList):
-    myRatingDict = {}
+def readRatingCSV():
     myFinalDict = {}
-    for item in nameList:
-        myFinalDict[item] = {}
-    #print(myFinalDict)
     fName = 'BX-Book-Ratings.csv'
     with open(fName, 'r') as inFile:
         line = inFile.readline()
         count = 0
         while line != "":
-            ratingList  = line.split(",")
-            val = ratingList[2].strip()
-            myFinalDict[ratingList[1]][ratingList[0]] = float(val)
-            #myFinalDict[ratingList[1]] ({ratingList[0] : float(val)})
-            #print(ratingList[0], ratingList[2])
-           #myList.append(nameList[0])
+        #while count < 5001:
+            cleanLine = str.replace(line, '"', "")
+            ratingList  = cleanLine.split(";")
+            if ratingList[1] != "ISBN":
+                if int(ratingList[0]) not in myFinalDict:
+                    myFinalDict[int(ratingList[0])] = {}
+                val = ratingList[2].strip()
+                myFinalDict[int(ratingList[0])][ratingList[1]] = float(val)
+            
             line = inFile.readline()
-            count = count + 1
+            #count = count + 1
     #print(count)
     return myFinalDict
 
 
 #------------------------------------Main
-bookList = readBookCSV()
-print(bookList)
+myInput = input("Enter Name: ").capitalize() #276746
+bookDict = readBookCSV()
+#print(bookDict)
+ratingDict = readRatingCSV()
+#print(ratingDict)
+print("Recommendations using Pearson:")
+rec = myRecommender().recommend(myInput, ratingDict, 2, True)
+#print(rec)
+#for item in rec:
+#    print(item[0], item[1])
+#    print(item[0], "--->", bookDict[item[0]], "--->",item[1] )
